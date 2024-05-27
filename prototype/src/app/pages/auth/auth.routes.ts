80fb410ce6c1d8c2f6811f74026e9336e7769932
Luiz Miguel <luizjarduli@gmail.com>
2024-05-27 10:52:11 -0300

    feat: added tailwind config, initial routing configuration

---
+++ prototype/src/app/pages/auth/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from "@angular/router";
+
+export const AUTH_ROUTES: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+]
